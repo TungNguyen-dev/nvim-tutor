53cfad553c6936d1e4596a60a87c7f4f0372ed1c
tungnn <tungnn@nal.vn>
2023-06-02 17:38:44 +0700

    Configure telescope

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -65,6 +65,10 @@ return packer.startup(function(use)
   use 'williamboman/mason.nvim'
   use 'williamboman/mason-lspconfig.nvim'
 
+  -- Telescope
+  use "nvim-telescope/telescope.nvim"
+  use 'nvim-telescope/telescope-media-files.nvim'
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
