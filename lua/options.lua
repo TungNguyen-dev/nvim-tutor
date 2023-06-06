c31c49f461eb386841b80959c728c2ca44edb7fb
tungnn <tungnn@nal.vn>
2023-06-06 18:23:02 +0700

    Set options: highlight column that in width: 80

--- lua/options.lua
+++ lua/options.lua
@@ -32,6 +32,7 @@ local options = {
   scrolloff = 8,                           -- is one of my fav
   sidescrolloff = 8,
   guifont = "monospace:h17",               -- the font used in graphical neovim applications
+  colorcolumn = "80",                       -- highlight column that in width of 80
 }
 
 vim.opt.shortmess:append "c"
