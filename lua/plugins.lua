32d7a3cdf2c21e01d579fd0f2352d42d82581917
tungnn <tungnn@nal.vn>
2023-06-06 18:11:19 +0700

    Configure gitsigns

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -82,6 +82,9 @@ return packer.startup(function(use)
   -- Autopairs
   use "windwp/nvim-autopairs" -- Autopairs, integrates with both cmp and treesitter
 
+  -- Git
+  use "lewis6991/gitsigns.nvim"
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
