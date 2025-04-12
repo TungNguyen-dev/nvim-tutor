e44e82d0c0ebd820f84fb77e8274d64b76ab8383
tungnn <tungnn.hn@gmail.com>
2025-04-12 16:21:33 +0700

    Options for java

---
+++ ftplugin/java.lua
@@ -0,0 +1,8 @@
+local options = {
+  shiftwidth = 4, -- the number of spaces inserted for each indentation.
+  tabstop = 4,    -- insert 2 spaces for a tab.
+}
+
+for k, v in pairs(options) do
+  vim.opt[k] = v
+end
