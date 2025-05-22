d30ac8637f22fc9e217c4d8c668e972aa1c8cd2f
tungnn <tungnn.hn@gmail.com>
2025-05-22 20:19:19 +0700

    Refresh my project

--- lua/plugin-config/lsp/init.lua
+++
@@ -1,7 +0,0 @@
-local status_ok, _ = pcall(require, "lspconfig")
-if not status_ok then
-  return
-end
-
-require "plugin-config.lsp.mason"
-require("plugin-config.lsp.handlers").setup()
--- lua/plugin-config/lsp/settings/intelephense.lua
+++
@@ -1,5 +0,0 @@
-return {
-  cmd = { "intelephense", "--stdio" },
-  filetypes = { "php" },
-  root_dir = root_pattern("composer.json", ".git"),
-}
