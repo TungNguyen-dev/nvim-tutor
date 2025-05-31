86df961503b3929884bbcc741277607cf7d67a2c
tungnn <tungnn.hn@gmail.com>
2025-05-31 16:45:43 +0700

    Install treesitter text-objects

--- lua/plugins/coding-support/syntax/treesitter.lua
+++ lua/plugins/coding-support/syntax/treesitter.lua
@@ -2,6 +2,9 @@ return {
   "nvim-treesitter/nvim-treesitter",
   version = "v0.9.3",
   build = ":TSUpdate",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter-textobjects"
+  },
   config = function()
     local configs = require("nvim-treesitter.configs")
     configs.setup({
