e5cf2e48637924c3a2b9b4aeb29873470f3076e6
tungnn <tungnn.hn@gmail.com>
2025-05-31 17:14:22 +0700

    Install leetcode

---
+++ lua/plugins/util/leetcode.lua
@@ -0,0 +1,15 @@
+return {
+  "kawre/leetcode.nvim",
+  build = ":TSUpdate html", -- if you have `nvim-treesitter` installed
+  dependencies = {
+    "nvim-telescope/telescope.nvim",
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+  },
+  opts = {
+    ---@type table<string, boolean>
+    plugins = {
+      non_standalone = false,
+    },
+  },
+}
