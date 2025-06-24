b5dc14c7fc0f273c4145bacb875ae65b9da182ed
tungnn <tungnn.hn@gmail.com>
2025-06-24 20:14:02 +0700

    Update style of java format by default google-java-format

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -5,11 +5,5 @@ return {
     formatters_by_ft = {
       java = { "google-java-format" },
     },
-    -- Customize formatters
-    formatters = {
-      ["google-java-format"] = {
-        prepend_args = { "--aosp" }, -- Ensure 4-space indentation
-      },
-    },
   },
 }
