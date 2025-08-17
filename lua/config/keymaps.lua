75a6f98b49c964981862f41640fc237e689ae77b
tungnn <tungnn.hn@gmail.com>
2025-08-17 14:12:57 +0700

    Update copilot and keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,8 +4,20 @@
 
 local wk = require("which-key")
 wk.add({
+  -- Insert mode
   {
-    mode = { "i" },
+    mode = "i",
     { "jk", "<ESC>", desc = "Exit insert mode" },
+  },
+  -- Normal mode window resizing and Neo-tree
+  {
+    mode = "n",
+    { "<A-Up>", ":resize +2<CR>", desc = "Resize window up", silent = true },
+    { "<A-Down>", ":resize -2<CR>", desc = "Resize window down", silent = true },
+    { "<A-Left>", ":vertical resize -2<CR>", desc = "Resize window left", silent = true },
+    { "<A-Right>", ":vertical resize +2<CR>", desc = "Resize window right", silent = true },
+    { "<leader>ec", "<cmd>Neotree position=current<CR>", desc = "Neo-tree: Open in current window" },
+    { "<leader>cc", "<cmd>CopilotChat<cr>", desc = "Open CopilotChat" },
+    { "<leader>ao", "<cmd>AerialOpen<cr>", desc = "Open Aerial" },
   },
 })
