cdaf2fd9cca77b322cef4b710d16a2a40e01aa9f
tungnn <tungnn.hn@gmail.com>
2025-10-15 20:02:09 +0700

    feat(plugins): add git, docker, sql, yaml treesitter
    - Added LazyVim extras for git, docker, sql, and yaml language support.
    - Added plugins: cmp-git, vim-dadbod, vim-dadbod-ui,
    vim-dadbod-completion.
    - Introduced custom :Cppath command to copy current file path to
    clipboard.

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -52,3 +52,10 @@ vim.api.nvim_create_user_command("FormatProject", function()
     ::continue::
   end
 end, {})
+
+-- Custom cmd to copy current file path to clipboard
+vim.api.nvim_create_user_command("Cppath", function()
+  local path = vim.fn.expand("%:p")
+  vim.fn.setreg("+", path)
+  vim.notify('Copied "' .. path .. '" to the clipboard!')
+end, {})
