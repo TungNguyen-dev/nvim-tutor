b77d743435fb298967f8221bbe727775a5667b55
tungnn <tungnn.hn@gmail.com>
2025-10-16 23:06:22 +0700

    feat(formatting): add prettier for multiple filetypes
    Added Prettier as the default formatter for JavaScript, TypeScript,
    JSON,
    HTML, CSS, and Markdown. Also set Prettier as the global formatter for
    all
    filetypes and added xmllint for XML. Updated lazyvim.json to include the
    prettier formatting plugin.

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -4,6 +4,19 @@ return {
     -- Define your formatters
     formatters_by_ft = {
       java = { "google-java-format" },
+      javascript = { "prettier" },
+      typescript = { "prettier" },
+      json = { "prettier" },
+      html = { "prettier" },
+      css = { "prettier" },
+      markdown = { "prettier" },
+      xml = { "xmllint" },
+
+      -- Use the "*" filetype to run formatters on all filetypes.
+      ["*"] = { "prettier" },
+      -- Use the "_" filetype to run formatters on filetypes that don't
+      -- have other formatters configured.
+      ["_"] = { "trim_whitespace" },
     },
   },
 }
